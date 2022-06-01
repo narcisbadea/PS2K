23e0414365fba5d394f236580673ba9cda77e892
narcisbadea <badeanarcis@gmail.ocm>
2022-06-01 21:40:22 +0300

    Cleaned wifi code

--- Wifi/wifi/wifi.ino
+++ Wifi/wifi/wifi.ino
@@ -7,15 +7,15 @@
 #define SERVER_WAYPOINTS "http://arduino.vhealth.me/Waypoints"
 
 #ifndef STASSID
-#define STASSID "iPhone - Narcis"
-#define STAPSK  "12345678"
+#define STASSID "iPhone - Sergiu"
+#define STAPSK  "sergiu181299"
 #endif
 Adafruit_INA219 ina219;
 float energy = 0;
 float shuntvoltage = 0;
 float busvoltage = 0;
 float current_mA = 0;
-float loadvoltage =0;
+float loadvoltage = 0;
 
 unsigned long timePUT = 0;
 unsigned long timeWay = 0; 
@@ -29,12 +29,6 @@ void setup() {
   }
 
   //Serial.println("Measuring voltage and current with INA219 ...");
-  
-  
-
-  //Serial.println();
-  //Serial.println();
-  //Serial.println();
 
   WiFi.begin(STASSID, STAPSK);
 
@@ -42,10 +36,6 @@ void setup() {
     delay(500);
     //Serial.print(".");
   }
-  //Serial.println("");
-  //Serial.print("Connected! IP address: ");
-  //Serial.println(WiFi.localIP());
-
 }
 
 void loop() {
@@ -63,9 +53,6 @@ void loop() {
       String payloadWaypoints = "";
       if (httpCode == HTTP_CODE_OK) {
           payloadWaypoints = http.getString();
-          //Serial.println("received payload:\n<<");
-          //Serial.println(payload);
-          //Serial.println(">>");
        }
        StaticJsonDocument<200> doc;
        char json[50];
@@ -83,10 +70,6 @@ void loop() {
       int td2 = doc[1]["td"];
   
       Serial. println((String)lr1 + " " + (String)lr2 + " " + (String) td1 + "  " + (String)td2);
-//      String way = (String)lr1 +";"+(String)lr2 +";"+(String)td1+";"+(String)td2;
-//      char wayChar[50];
-//      way.toCharArray(wayChar,50);
-//      Serial.write(wayChar, 50);
     }
     
     //=====================================================================
@@ -98,29 +81,20 @@ void loop() {
       loadvoltage = busvoltage + (shuntvoltage / 1000);
       energy = energy + loadvoltage * current_mA / 3600;
     
-
-      //Serial.print("[HTTP] begin...\n");
-      // configure traged server and url
       http.begin(client, SERVER_IP); //HTTP
       http.addHeader("Content-Type", "application/json");
-      //Serial.print("[HTTP] POST...\n");
+      
       // start connection and send HTTP header and body
       String post = "{\n  \"livePower\": "+(String)(ina219.getPower_mW()/1000)+",\n  \"totalPower\": "+(String)(energy)+"\n}";
-      //Serial.println(post);
-      //String post = "{\"wh\": "+(String)ina219.getPower_mW()+"}";
+      
       httpCode = http.PUT(post);
       
       // httpCode will be negative on error
       if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        //Serial.printf("[HTTP] POST... code: %d\n", httpCode);
   
         // file found at server
         if (httpCode == HTTP_CODE_OK) {
           const String& payload = http.getString();
-          //Serial.println("received payload:\n<<");
-          //Serial.println(payload);
-          //Serial.println(">>");
         }
       } else {
         //Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
